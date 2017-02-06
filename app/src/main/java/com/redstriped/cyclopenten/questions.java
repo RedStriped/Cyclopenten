e9071c63491a703656429de76c3196f6a74732e5
Malte <malte.grebe@hotmail.de>
2017-02-06 06:58:49 +0100

    Initial commit

---
+++ app/src/main/java/com/redstriped/cyclopenten/questions.java
@@ -0,0 +1,21 @@
+package com.redstriped.cyclopenten;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.cyclopenten.redstriped.cyclopenten.R;
+
+public class questions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+    }
+
+
+}
